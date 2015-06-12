fee6f39f65eab6ed609b912ad8417bf05dcc703b
sparrow.jang <sparrow.jang@gmail.com>
2015-06-12 14:10:42 +0800

    added keywords and modified the url

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ except:
     from distutils.core import setup, find_packages
                         
 setup(                  
-    version="0.0.2",
+    version="0.0.3",
     description="Let you convert any image size.",
     author="sparrow",    
     author_email="sprrow.jang@gmail.com",
-    url="http://sparrowhome.twbbs.org/",
+    url="https://github.com/SparrowJang/image_converter",
     name='image_converter',
+    keywords="image convert resize",
     packages=find_packages(),
     install_requires=["pil"] 
 )      
