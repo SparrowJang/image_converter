049e8734f208a39648fc0e6750b219c86a4c5906
Sparrow.jang <sparrow.jang@gmail.com>
2015-07-07 10:09:59 +0800

    added a license doc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
     from distutils.core import setup, find_packages
                         
 setup(                  
-    version="0.0.3",
+    version="0.0.4",
     description="Let you convert any image size.",
     author="sparrow",    
     author_email="sprrow.jang@gmail.com",
